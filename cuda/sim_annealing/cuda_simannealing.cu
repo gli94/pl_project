43ea57b2d3b4eded160216273a3f9871c3d23329
GL <GL@wireless-10-145-82-78.public.utexas.edu>
2019-05-04 00:11:53 -0500

    cuda sim annealing works

--- cuda/sim_annealing/cuda_simannealing.cu
+++ cuda/sim_annealing/cuda_simannealing.cu
@@ -747,7 +747,7 @@ void callSAKernel (const int blocksPerGrid,
 
 void cuda_SimAnnealing(int * board, int * solved)
 {
-    int blocksPerGrid = 2048;
+    int blocksPerGrid = 1024;
     int threadsPerBlock = 256;
     
     curandState* devStates;
