a34cb686e9d873649abf99f2654b638e95045d57
GL <GL@wireless-10-145-65-85.public.utexas.edu>
2019-05-12 18:03:44 -0500

    Cuda stats collection

--- cuda/backtracking/stats_collection/main.cc
+++ cuda/backtracking/stats_collection/main.cc
@@ -60,7 +60,29 @@ int main()
     
     double execution_time[NUM_TESTCASE];
     
-    load(filename, data);
+    //load(filename, data);
+    
+    char buf[MAXB] = {0};
+    
+    int i = 0;
+    int j = 0;
+    
+    FILE * file;
+    file = fopen(filename, "r");
+    
+    if (file == 0)
+    {
+        fprintf(stderr, "failed to open test.txt\n");
+        return 1;
+    }
+    
+    while (i < MAXL && fgets (buf, MAXB - 1, file))
+    {
+        sscanf(buf, "%d %d %d %d %d %d %d %d %d", &data[i * N + 0], &data[i * N + 1], &data[i * N + 2], &data[i * N + 3], &data[i * N + 4], &data[i * N + 5], &data[i * N + 6], &data[i * N + 7], &data[i * N + 8]);
+        i++;
+    }
+    
+    fclose(file);
     
     int *solved = new int[N * N];
     
