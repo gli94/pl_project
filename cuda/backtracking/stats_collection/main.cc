51eeaed2435753203dc07073b6002385d7fdaa5f
GL <GL@wireless-10-146-135-91.public.utexas.edu>
2019-05-14 10:09:09 -0500

    cuda stats collection

--- cuda/backtracking/stats_collection/main.cc
+++ cuda/backtracking/stats_collection/main.cc
@@ -53,7 +53,7 @@ void printBoard(int *board) {
 int main()
 {
     
-    char filename[] = "sudoku_9x9_100_56.txt";
+    char filename[] = "sudoku_9x9_100_64.txt";
     
     int *data = new int [MAXL * N];
     
@@ -89,7 +89,7 @@ int main()
         printf("\n");
     }
     
-    FILE * f = fopen("bt_stats_bench_9x9_100_56.txt", "w");
+    FILE * f = fopen("bt_stats_bench_9x9_100_64.txt", "w");
     
     if (f == NULL)
     {
