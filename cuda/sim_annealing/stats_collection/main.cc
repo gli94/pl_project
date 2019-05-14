f1a95a5b2796b4f13e63e27bfa83840c3e8b488e
GL <GL@wireless-10-146-135-91.public.utexas.edu>
2019-05-14 10:43:25 -0500

    cuda stats collection

--- cuda/sim_annealing/stats_collection/main.cc
+++ cuda/sim_annealing/stats_collection/main.cc
@@ -52,7 +52,7 @@ void printBoard(int *board) {
 
 int main()
 {
-    char filename[] = "sudoku_9x9_100_32.txt";
+    char filename[] = "sudoku_9x9_100_40.txt";
     
     int *data = new int [MAXL * N];
     
@@ -88,7 +88,7 @@ int main()
         printf("\n");
     }
     
-    FILE * f = fopen("bt_stats_bench_9x9_100_32.txt", "w");
+    FILE * f = fopen("bt_stats_bench_9x9_100_40.txt", "w");
     
     if (f == NULL)
     {
