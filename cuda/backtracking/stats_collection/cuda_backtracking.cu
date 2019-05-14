7bb00584016ca48ceb49bbd074bf717beb7598aa
GL <GL@wireless-10-146-135-91.public.utexas.edu>
2019-05-14 09:57:42 -0500

    cuda stats collection

--- cuda/backtracking/stats_collection/cuda_backtracking.cu
+++ cuda/backtracking/stats_collection/cuda_backtracking.cu
@@ -629,7 +629,7 @@ void cuda_Backtrack(int * board, int * solved, double *exec_time)
     
     int host_count;
     
-    int iterations = 20;
+    int iterations = 24;
     
     for (int i=0; i<iterations; i++)
     {
